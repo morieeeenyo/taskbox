d0c56743d2cc372c7d88f7795d8ba30938043a93
togo-mentor <uint2435@gmail.com>
2022-01-11 08:03:54 +0900

    UI調整

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="list-items">
     <template v-if="loading">
-      loading
+      <div v-for="n in 6" :key="n" class="loading-item">
+        <span class="glow-checkbox" />
+        <span class="glow-text">
+          <span>Loading</span> <span>cool</span> <span>state</span>
+        </span>
+      </div>
     </template>
     <template v-else-if="isEmpty">
-      empty
+      <div class="wrapper-message">
+        <span class="icon-check" />
+        <div class="title-message">You have no tasks</div>
+        <div class="subtitle-message">Sit back and relax</div>
+      </div>
     </template>
     <template v-else>
       <Task
-        v-for="task in tasks"
+        v-for="task in tasksInOrder"
         :key="task.id"
         :task="task"
         @archive-task="onArchiveTask"
@@ -19,35 +28,41 @@
 </template>
 
 <script>
-  import Task from './Task';
-  import { reactive, computed } from 'vue';
+import Task from "./Task";
+import { reactive, computed } from "vue";
 
-  export default {
-    name: 'TaskList',
-    components: { Task },
-    props: {
-      tasks: { type: Array, required: true, default: () => [] },
-      loading: { type: Boolean, default: false },
-    },
-    emits: ['archive-task', 'pin-task'],
+export default {
+  name: "TaskList",
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true, default: () => [] },
+    loading: { type: Boolean, default: false },
+  },
+  emits: ["archive-task", "pin-task"],
 
-    setup(props, { emit }) {
-      props = reactive(props);
-      return {
-        isEmpty: computed(() => props.tasks.length === 0), // tasksが空のときtrue
-        /**
-         * Event handler for archiving tasks
-         */
-        onArchiveTask(taskId) {
-          emit('archive-task', taskId);
-        },
-        /**
-         * Event handler for pinning tasks
-         */
-        onPinTask(taskId) {
-          emit('pin-task', taskId);
-        },
-      };
-    },
-  };
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      isEmpty: computed(() => props.tasks.length === 0), // tasksが空のときtrue
+      tasksInOrder: computed(() => {
+        return [
+          ...props.tasks.filter((t) => t.state === "TASK_PINNED"), // ピン留めが先頭に来るようにする
+          ...props.tasks.filter((t) => t.state !== "TASK_PINNED"),
+        ];
+      }),
+      /**
+       * Event handler for archiving tasks
+       */
+      onArchiveTask(taskId) {
+        emit("archive-task", taskId);
+      },
+      /**
+       * Event handler for pinning tasks
+       */
+      onPinTask(taskId) {
+        emit("pin-task", taskId);
+      },
+    };
+  },
+};
 </script>
